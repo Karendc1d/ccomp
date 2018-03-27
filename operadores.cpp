497777b606eaeae099507f07da0486b130054491
ALUMNO-G <ALUMNO-G@UCSP.AULAB>
2018-03-27 09:14:03 -0500

    wuuut

---
+++ operadores.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    int a=5;
+    int b=6;
+    int c=9;
+    int e=10;
+    int f=2;
+    int q=1;
+    int d=a+b*c;
+    int m= a+b+c+e+f/5;
+    int p=a*b%c + e/f -q;
+    cout <<m<<endl;
+   cout<<d<<endl;
+   cout<<p<<endl;
+    return 0;
+}
