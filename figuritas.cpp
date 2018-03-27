f4a6379858b2ef15fb83757bfc7bde027e2d2f30
ALUMNO-G <ALUMNO-G@UCSP.AULAB>
2018-03-27 08:55:03 -0500

    karenDe

---
+++ figuritas.cpp
@@ -0,0 +1,73 @@
+/*#include <iostream>
+#include <string>
+#include <cmath>
+using namespace std;
+
+int main(){
+    foat x=13;
+    float y=6;
+    float z=x+y;
+    int multiplicacion=x*y;
+    float division=x/y;
+    string b= "Hola mundo";
+    string s= "Hello world";
+    cout<<"El primer numero es "<<x<<", el segundo numero es "<<y<<" ...la suma es "<<z<<" la multiplicacion es "<<multiplicacion<<" y la division es "<< division<<endl;
+    cout <<b<<endl;
+    cout <<s<<endl;
+
+
+    //Radio de un círculo
+    float radio;
+    cout<<"Ingrese el radio del circulo:";
+    cin>>radio;
+    float area=3.14*pow(radio,2);
+    cout<<"El area del circulo de radio "<<radio<<" es "<<area<<endl;
+    //Simbología
+
+    cout <<"hola mundo\n";
+    cout<<" Karen"<<endl;
+
+    //ejercicio 1
+
+    int c=0;
+    string a= "**********";
+    string b= "*        *";
+    cout <<a<<endl;
+
+    while (c<=6){
+        cout<<b<<endl;
+    c=c+1;
+    }
+    cout <<a<<endl;
+
+
+
+
+    return 0;
+}
+
+*/
+
+#include <iostream>
+#include <string>
+#include <cmath>
+using namespace std;
+
+int main(){
+    string a= "**********          ***            *";
+    string d= "*        *        *     *         * *";
+    string b= "*        *       *       *       *   *";
+    string c= "*        *       *       *      *     *";
+    string e= "*        *       *       *     *       *";
+    string f= "*        *        *     *     *         *";
+    string g= "**********          ***      *************";
+
+    cout <<a<<endl;
+    cout <<d<<endl;
+    cout <<b<<endl;
+    cout <<c<<endl;
+    cout <<e<<endl;
+    cout <<f<<endl;
+    cout <<g<<endl;
+
+return 0;}
