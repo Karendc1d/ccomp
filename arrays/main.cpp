f9fe0738a313ab08065a97220215d8d546dd5a46
Karen Denegri <31105002+Karendc1d@users.noreply.github.com>
2018-04-24 10:01:04 -0500

    Update main.cpp

--- arrays/main.cpp
+++ arrays/main.cpp
@@ -2,64 +2,89 @@
 
 using namespace std;
 
+//
+//int suma(int a[]){
+//int c=0;
+//for(int i=0; a[i]!='\0';i++){
+//    c+=a[i];
+//}
+//return c;
+//}
+//
+//int sumaRecursiva(int array[],int largo)
+//{
+//    int suma=0;
+//    if(largo==1)
+//        {
+//        suma+=array[0];
+//        }
+//    else
+//        {
+//        suma=array[largo-1]+sumaRecursiva(array,largo-1);
+//    }
+//}
+//
+//
+//
+//void strcpy(char a[],char b[]){
+//for (int i=0; b[i]!='\0';i++){
+//    a[i]=b[i];
+// cout<<a;
+//}
+//
+//
+//
+//
+//char strcat(char s[],char t[] ){
+//int b=0;
+//for (int i=0;s[i]!='\0';i++){
+//    b++;
+//}
+//for(int i=0;t[i]!='\0';i++){
+//    s[++b]=t[i];
+//}
+//;
+//
+//}
+//
+//int tamanio(char a[]){
+//int c=1;
+//for(int i=1;a[i]!='\0';i++){
+//    c++;
+//}
+//return c;
+//
+//}
 
-int suma(int a[]){
-int c=0;
-for(int i=0; a[i]!='\0';i++){
-    c+=a[i];
-}
-return c;
-}
+void invertir(int tam,int a[]){
+int c=tam-1;
+int b=a[0];
+for (int i=0;a[i]!=tam/2;i++){
 
-int sumaRecursiva(int array[],int largo)
-{
-    int suma=0;
-    if(largo==1)
-        {
-        suma+=array[0];
-        }
-    else
-        {
-        suma=array[largo-1]+sumaRecursiva(array,largo-1);
-    }
-}
+    a[i]=a[c];
+    a[c]=a[b];
+    c--;
+    b++;
 
-
-
-
-void strcpy(char a[],char b[]){
-for (int i=0; b[i]!='\0';i++){
-    a[i]=b[i];
 }
 cout<<a;
 }
 
 
-
-
-char strcat(char s[],char t[] ){
-int b=0;
-for (int i=0;s[i]!='\0';i++){
-    b++;
-}
-for(int i=0;t[i]!='\0';i++){
-    s[++b]=t[i];
-}
-;
-
-}
-
 int main()
 {
     char cad1[20]={'1','2','3','4','5'};
     char cad2[]={'6','7','8','9'};
-    int arr[]={1,2,3,4,5,'\0'};
+    int arr[]={1,2,3,4,5,6,'\0'};
 
 
-    cout<<strcat(cad1,cad2);
-    strcpy(cad1,cad2);
-    cout<<suma(arr);
-    cout<<sumaRecursiva(arr;5);
+//    cout<<strcat(cad1,cad2);
+//    strcpy(cad1,cad2);
+     invertir(6,arr);
+    // cout<<suma(arr);
+    //cout<<sumaRecursiva(arr,6);
+    //cout<<tamanio(cad1);
+
     return 0;
 }
 
