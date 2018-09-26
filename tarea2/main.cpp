1aebd411cd3b4c0dbfd022295cf4d070ab01a365
Karen Denegri <31105002+Karendc1d@users.noreply.github.com>
2018-09-25 22:19:10 -0500

    Update main.cpp

--- tarea2/main.cpp
+++ tarea2/main.cpp
@@ -40,6 +40,62 @@ return 0;
 }
 
 
+
+/*
+#include <iostream>
+using namespace std;
+
+void separar(char* palabra) {
+  for(int i=0;palabra[i]!='\0';i++) {
+//for(int j=0;nuevo[j]!='\0';j++){
+    cout<<palabra[i];
+//Vocal y consonante
+    if((palabra[i]=='a' || palabra[i]=='e' || palabra[i]=='o') && (palabra[i+1]!='a' || palabra[i+1]!='e' || palabra[i+1]!='i' || palabra[i+1]!='o' || palabra[i+1]!='u')) {
+      //si no hay diptongo entonces coloca el -
+      if(palabra[i+2]!='a'||palabra[i+2]!='e'||palabra[i+2]!='i'||palabra[i+2]!='o'||palabra[i+2]!='u') {
+        cout<<palabra[i+1];
+        cout<<'-';
+        i++;
+      }
+      else{
+        cout<<'-';
+      }
+    }
+//Vocalfuerte  y
+    if((palabra[i]=='a'||palabra[i]=='e'||palabra[i]=='o')&&(palabra[i++]!='i'||palabra[i++]!='u')){//junta los diptongos
+      cout<<palabra[i];
+      cout<<'-';
+    }
+//    }
+  }
+}
+
+
+/void separar(char a) {
+  for(char* p=a; *p; p++) {
+    if((*p=='a' || *p=='e' || *p=='i' || *p=='o' || *p=='u')){
+      cout<<*p<<'-';
+    }
+    else
+      cout<<*p;
+  }
+}*/
+
+int main() {
+  char palabra [30];
+  char nuevo [50];
+  cout << "Ingrese una palabra: ";
+  cin >> palabra;
+  separar(palabra);
+
+return 0;
+}
+*/
+
+
+
+
+
 //
 //void separar(char a[],char b[]){
 //
